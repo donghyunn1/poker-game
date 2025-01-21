dc7a3da7719aa903c334916cf8dfba2119826d6d
donghyunn1 <dongdong0516@naver.com>
2025-01-21 13:05:05 +0900

    feat: 가지고 있는 패의 rankType 구별하는 정보 구현

---
+++ src/HandRank.java
@@ -0,0 +1,33 @@
+import java.util.List;
+
+public class HandRank implements Comparable<HandRank>{
+
+    private HandRankType type;
+    private List<Card> cards;
+
+    public HandRank(HandRankType type, List<Card> cards) {
+        this.type = type;
+        this.cards = cards;
+    }
+
+    public HandRankType getType() {
+        return type;
+    }
+
+    public List<Card> getCards() {
+        return cards;
+    }
+
+    @Override
+    public String toString() {
+        return getType().getRankName();
+    }
+
+    @Override
+    public int compareTo(HandRank other) {
+        if (this.getType().getScore() != other.getType().getScore()) {
+            return this.getType().getScore() - other.getType().getScore();
+        }
+        //todo: RankType이 같을 경우 로직 ?
+    }
+}
